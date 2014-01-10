c668604f94a974a291eea6fe28f92cdcfdb89725
Terry Biberdorf <terry.biberdorf@symcom.com>
2014-01-10 13:52:12 -0700

    change clock settings to match ATS h/w

--- Sources/app/main_kinetis.c
+++ Sources/app/main_kinetis.c
@@ -1576,6 +1576,7 @@ void set_MCG_PEE_Mode( void ) {
  ****************************************************************************/
 static int pll_init()
 {
+#if TWR_K60D
 	/* 
 	 * First move to FBE mode
 	 * Enable external oscillator, RANGE=0, HGO=, EREFS=, LP=, IRCS=
@@ -1626,7 +1627,55 @@ static int pll_init()
 
 	/* Wait for LOCK bit to set */
 	while (!(MCG_S & MCG_S_LOCK_MASK)) {};
+#else
+	SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK   /* Enable clock gate for ports to enable pin routing */
+			| SIM_SCGC5_PORTB_MASK
+			| SIM_SCGC5_PORTC_MASK
+			| SIM_SCGC5_PORTD_MASK
+			| SIM_SCGC5_PORTE_MASK;
 
+	SIM_CLKDIV1 = SIM_CLKDIV1_OUTDIV1(0x00) |
+	                SIM_CLKDIV1_OUTDIV2(0x00) |
+	                SIM_CLKDIV1_OUTDIV3(0x00) |
+	                SIM_CLKDIV1_OUTDIV4(0x01); /* Update system prescalers */
+	  /* SIM_SOPT2: PLLFLLSEL=1 */
+	  SIM_SOPT2 |= SIM_SOPT2_PLLFLLSEL_MASK; /* Select PLL as a clock source for various peripherals */
+	  /* SIM_SOPT1: OSC32KSEL=3 */
+//	  SIM_SOPT1 |= SIM_SOPT1_OSC32KSEL(0x03); /* LPO 1kHz oscillator drives 32 kHz clock for various peripherals */
+	  /* PORTA_PCR18: ISF=0,MUX=0 */
+	  PORTA_PCR18 &= (uint32_t)~(uint32_t)((PORT_PCR_ISF_MASK | PORT_PCR_MUX(0x07)));                                   
+	  /* PORTA_PCR19: ISF=0,MUX=0 */
+	  PORTA_PCR19 &= (uint32_t)~(uint32_t)((PORT_PCR_ISF_MASK | PORT_PCR_MUX(0x07)));                                   
+	  /* Switch to FBE Mode */
+	  /* MCG_C2: LOCRE0=0,??=0,RANGE0=2,HGO0=0,EREFS0=1,LP=0,IRCS=0 */
+	  MCG_C2 = (MCG_C2_RANGE(0x02) | MCG_C2_EREFS_MASK);                                   
+	  /* OSC_CR: ERCLKEN=1,??=0,EREFSTEN=0,??=0,SC2P=0,SC4P=0,SC8P=0,SC16P=0 */
+	  OSC_CR = OSC_CR_ERCLKEN_MASK;                                   
+	  /* MCG_C7: OSCSEL=0 */
+//	  MCG_C7 &= (uint8_t)~(uint8_t)(MCG_C7_OSCSEL_MASK);                                   
+	  /* MCG_C1: CLKS=2,FRDIV=5,IREFS=0,IRCLKEN=1,IREFSTEN=0 */
+	  MCG_C1 = (MCG_C1_CLKS(0x02) | MCG_C1_FRDIV(0x05) | MCG_C1_IRCLKEN_MASK);                                   
+	  /* MCG_C4: DMX32=0,DRST_DRS=0 */
+	  MCG_C4 &= (uint8_t)~(uint8_t)((MCG_C4_DMX32_MASK | MCG_C4_DRST_DRS(0x03)));                                   
+	  /* MCG_C5: ??=0,PLLCLKEN0=0,PLLSTEN0=0,PRDIV0=0x0B */
+	  MCG_C5 = MCG_C5_PRDIV(0x0B);                                   
+	  /* MCG_C6: LOLIE0=0,PLLS=0,CME0=0,VDIV0=0 */
+	  MCG_C6 = MCG_C6_VDIV(0x00);                                   
+	  while((MCG_S & MCG_S_OSCINIT_MASK) == 0x00U) { /* Check that the oscillator is running */
+	  }
+	  while((MCG_S & MCG_S_IREFST_MASK) != 0x00U) { /* Check that the source of the FLL reference clock is the external reference clock. */
+	  }
+	  while((MCG_S & 0x0CU) != 0x08U) {    /* Wait until external reference clock is selected as MCG output */
+	  }
+	  /* Switch to PBE Mode */
+	  /* MCG_C6: LOLIE0=0,PLLS=1,CME0=0,VDIV0=0 */
+	  MCG_C6 = (MCG_C6_PLLS_MASK | MCG_C6_VDIV(0x00));                                   
+	  while((MCG_S & 0x0CU) != 0x08U) {    /* Wait until external reference clock is selected as MCG output */
+	  }
+	  while((MCG_S & MCG_S_LOCK_MASK) == 0x00U) { /* Wait until locked */
+	  }
+
+#endif
 	/* Now running PBE Mode */
 	set_MCG_PEE_Mode(); 
 
